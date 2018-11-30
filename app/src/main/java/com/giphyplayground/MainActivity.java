486f4efce0c66712f3b981a0076c9dba611117a2
Jonathan Ma <jonma1221@gmail.com>
2018-11-29 17:01:25 -0800

    added retrofit, Giphy API key, and get interface

--- app/src/main/java/com/giphyplayground/MainActivity.java
+++ app/src/main/java/com/giphyplayground/MainActivity.java
@@ -2,12 +2,45 @@ package com.giphyplayground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.giphyplayground.network.service.GiphyGetService;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String BASE_URL = "http://api.giphy.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    //GetDataService service =
+        // RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GiphyGetService giphyGetService = retrofit.create(GiphyGetService.class);
+        Call<ResponseBody> call = giphyGetService.getTrending(BuildConfig.GiphyApiKey);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Log.d("[Response]", response.toString());
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.d("[Response]", "failed");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/giphyplayground/network/service/GiphyGetService.java
@@ -0,0 +1,12 @@
+package com.giphyplayground.network.service;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GiphyGetService {
+
+    @GET("/v1/gifs/trending")
+    Call<ResponseBody> getTrending(@Query("api_key") String apiKey);
+}
